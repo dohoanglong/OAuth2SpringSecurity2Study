0bdc8aea4d20a9aaf93aafe1f1092f94b105f89b
Long Hoang Do <longhoangdo@kms-technology.com>
2023-01-27 14:50:53 +0700

    Delete comments, change version, add secret key configuration

--- src/main/java/com/practice/auth/SecurityConfiguration.java
+++ src/main/java/com/practice/auth/SecurityConfiguration.java
@@ -6,21 +6,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration(proxyBeanMethods = false)
-public class SecurityConfig {
+public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        //        http.authorizeHttpRequests(auth -> auth
-        //                .requestMatchers("/", "/error", "/webjars/**").permitAll()
-        //                .anyRequest().authenticated())
-        //            .exceptionHandling(
-        //                e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
-        //            .oauth2Login()
-        //            .and().logout(l -> l
-        //                .logoutSuccessUrl("/").permitAll());
-
         http.authorizeHttpRequests(
             (request) -> request.requestMatchers("/", "/error", "/webjars/**","/index.html/**").permitAll()
                 .anyRequest().authenticated());
@@ -28,7 +18,6 @@ public class SecurityConfig {
             e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
         http.oauth2Login().and().logout(l -> l
             .logoutSuccessUrl("/").permitAll());
-        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 
         return http.build();
     }
